7bede4b0aa113893804b7c414706bc82e6c3af46
Amgad Malik <amgadmalik@yahoo.com>
2019-12-27 16:29:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,12 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-get '/' do
-      erb :index
-    end
- 
-    get "/info" do
-      erb :info
-    end
+  get '/' do
+    erb :index
+  end
 
 end
+
